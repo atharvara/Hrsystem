e2b15a1130371346cb80b894c9410ea4e7a933c5
Atharva <atharvar40@gmail.com>
2021-03-21 16:01:30 +0530

    request page work

---
+++ app/src/main/java/com/example/hrsystem/requesttask/requestedtask/RequestPage.java
@@ -0,0 +1,15 @@
+package com.example.hrsystem.requesttask.requestedtask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.hrsystem.R;
+
+public class RequestPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_page2);
+    }
+}
